bbb0948d95d4806af0cc7871ac2410a1aabeca96
ymiru0324 <ymiru0324@naver.com>
2023-03-19 00:20:26 +0900

    update: Spring - Vue 연동 테스트

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>테스트테스트테스트</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
